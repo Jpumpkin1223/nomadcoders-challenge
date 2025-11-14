537ed78f69172aabfc5f24d0de50985b4dc570a9
Jpumpkin1223 <jpark9595@naver.com>
2025-11-14 22:39:57 +0900

    Configure admin for Tweet and Like models

--- airbnb-challenge-1/tweets/admin.py
+++ airbnb-challenge-1/tweets/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Tweet, Like
 
-# Register your models here.
+
+@admin.register(Tweet)
+class TweetAdmin(admin.ModelAdmin):
+    list_display = ['id', 'payload', 'user', 'created_at', 'updated_at']
+    list_filter = ['created_at', 'updated_at', 'user']
+    search_fields = ['payload', 'user__username']
+    readonly_fields = ['created_at', 'updated_at']
+    date_hierarchy = 'created_at'
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'tweet', 'created_at', 'updated_at']
+    list_filter = ['created_at', 'updated_at', 'user']
+    readonly_fields = ['created_at', 'updated_at']
+    date_hierarchy = 'created_at'
