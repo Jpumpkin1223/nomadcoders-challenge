584dfebc44013e5f3cacf2df2da0468ed548a486
Jpumpkin1223 <jpark9595@naver.com>
2025-11-14 22:49:59 +0900

    Refactor: Move TimeStampedModel to common app

---
+++ airbnb-challenge-1/common/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class TimeStampedModel(models.Model):
+    """추상 기본 클래스: created_at과 updated_at 필드를 제공"""
+
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    class Meta:
+        abstract = True
