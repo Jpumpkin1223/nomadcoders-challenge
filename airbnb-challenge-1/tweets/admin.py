6b9fbadcd685dd641d5ed788f54c7ef2601e337f
Jpumpkin1223 <jpark9595@naver.com>
2025-11-17 22:15:47 +0900

    Add like count to tweet admin

--- airbnb-challenge-1/tweets/admin.py
+++ airbnb-challenge-1/tweets/admin.py
@@ -5,11 +5,23 @@ from .models import Like, Tweet
 
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
-    list_display = ["id", "payload", "user", "created_at", "updated_at"]
+    list_display = [
+        "id",
+        "payload",
+        "user",
+        "like_count",
+        "created_at",
+        "updated_at",
+    ]
     list_filter = ["created_at", "updated_at", "user"]
     search_fields = ["payload", "user__username"]
     readonly_fields = ["created_at", "updated_at"]
     date_hierarchy = "created_at"
+
+    def like_count(self, obj):
+        return obj.likes.count()
+
+    like_count.short_description = "Likes"
 
 
 @admin.register(Like)
