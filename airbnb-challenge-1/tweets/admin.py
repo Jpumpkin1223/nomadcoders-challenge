ef3d2d5aa02e5f38ed2e58e4bdc6f99055c774bd
Jpumpkin1223 <jpark9595@naver.com>
2025-11-19 05:52:33 +0900

    Add username search field to LikeAdmin

--- airbnb-challenge-1/tweets/admin.py
+++ airbnb-challenge-1/tweets/admin.py
@@ -28,5 +28,6 @@ class TweetAdmin(admin.ModelAdmin):
 class LikeAdmin(admin.ModelAdmin):
     list_display = ["id", "user", "tweet", "created_at", "updated_at"]
     list_filter = ["created_at", "updated_at", "user"]
+    search_fields = ["user__username"]
     readonly_fields = ["created_at", "updated_at"]
     date_hierarchy = "created_at"
