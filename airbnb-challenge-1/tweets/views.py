af43c17d04c6be9c8cf3171b0829f6edd0b47242
holden <holden.park@jungfrau.co.kr>
2025-11-20 18:31:46 +0900

    Implement API views for tweets list and user tweets list with User.DoesNotExist handling

--- airbnb-challenge-1/tweets/views.py
+++ airbnb-challenge-1/tweets/views.py
@@ -1,9 +1,37 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Tweet
+from .serializers import TweetSerializer
 
 
 def tweet_list(request):
     """모든 Tweets를 보여주는 뷰"""
     tweets = Tweet.objects.all()
     return render(request, "tweets/list.html", {"tweets": tweets})
+
+
+@api_view(["GET"])
+def api_tweet_list(request):
+    """모든 Tweets를 반환하는 API 뷰"""
+    tweets = Tweet.objects.all()
+    serializer = TweetSerializer(tweets, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(["GET"])
+def api_user_tweet_list(request, user_id):
+    """특정 User의 모든 Tweets를 반환하는 API 뷰"""
+    try:
+        user = User.objects.get(id=user_id)
+    except User.DoesNotExist:
+        return Response(
+            {"error": "User not found"}, status=status.HTTP_404_NOT_FOUND
+        )
+
+    tweets = Tweet.objects.filter(user=user)
+    serializer = TweetSerializer(tweets, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
