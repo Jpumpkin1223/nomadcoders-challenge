7119c547038bf23987a1d3a83f4408f880a00b87
Jpumpkin1223 <jpark9595@naver.com>
2025-11-21 23:14:50 +0900

    refactor: Change TweetSerializer to use ModelSerializer

--- airbnb-challenge-1/tweets/serializers.py
+++ airbnb-challenge-1/tweets/serializers.py
@@ -3,13 +3,14 @@ from rest_framework import serializers
 from .models import Tweet
 
 
-class TweetSerializer(serializers.Serializer):
+class TweetSerializer(serializers.ModelSerializer):
     """Tweet 모델을 직렬화하는 Serializer"""
 
-    id = serializers.IntegerField(read_only=True)
-    payload = serializers.CharField(max_length=180)
     user = serializers.IntegerField(source="user.id", read_only=True)
     username = serializers.CharField(source="user.username", read_only=True)
-    created_at = serializers.DateTimeField(read_only=True)
-    updated_at = serializers.DateTimeField(read_only=True)
+
+    class Meta:
+        model = Tweet
+        fields = ["id", "payload", "user", "username", "created_at", "updated_at"]
+        read_only_fields = ["id", "user", "username", "created_at", "updated_at"]
 
